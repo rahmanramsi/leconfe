58b5f17fa08d5e2ec1c4e2c8cc0523ef9e2a93be
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-08-03 15:20:50 +0800

    add detail link

--- plugins/DefaultSidebar/views/sidebar/timeline.blade.php
+++ plugins/DefaultSidebar/views/sidebar/timeline.blade.php
@@ -1,3 +1,8 @@
 <div class="w-full">
     <div x-calendar="{{ json_encode($timelines) }}" class="mx-auto"></div>
+    <div class="flex justify-end w-full pt-1">
+        <a href="{{ route('livewirePageGroup.scheduledConference.pages.timelines') }}" class="btn btn-primary btn-sm">
+            More
+        </a>
+    </div>
 </div>
