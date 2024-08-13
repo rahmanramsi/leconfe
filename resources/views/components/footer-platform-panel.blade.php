9ea182f0c1e93c4acddc857a3f3a12e53694f81f
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-08-13 14:08:50 +0800

    optimize footer platform on larger screen

--- resources/views/components/footer-platform-panel.blade.php
+++ resources/views/components/footer-platform-panel.blade.php
@@ -1,5 +1,5 @@
 <div class="platform-footer">
-	<div class="max-w-7xl mx-auto p-4 flex">
+	<div class="mx-auto h-full w-full px-4 md:px-6 lg:px-8 max-w-full flex">
 		<a href="https://leconfe.com" target="_blank" class="ms-auto">
 			<img src="{{ Vite::asset('resources/assets/images/footer.png') }}" class="h-20" alt="leconfe-logo-footer">
 		</a>
