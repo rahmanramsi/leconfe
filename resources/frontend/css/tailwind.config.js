1a6e9adf111a7d995a619c7fc21eca16b527ddd1
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-08-14 14:21:29 +0800

    dont style table by default

--- resources/frontend/css/tailwind.config.js
+++ resources/frontend/css/tailwind.config.js
@@ -25,6 +25,9 @@ export default {
                     a: {
                         'text-decoration': 'none',
                     },
+                    tr: {
+                        'border-bottom-width': '0px',
+                    },
                   },
                 },
               }),
