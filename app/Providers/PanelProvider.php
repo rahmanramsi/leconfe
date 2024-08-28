8c33714f45c6f1ea32e92a83f15b1fbee2f1de7d
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-08-28 14:54:19 +0800

    improve error handling

--- app/Providers/PanelProvider.php
+++ app/Providers/PanelProvider.php
@@ -40,15 +40,15 @@ class PanelProvider extends ServiceProvider
         $this->setupPanel($panel)
             ->id(static::PANEL_SCHEDULED_CONFERENCE)
             ->path('{conference:path}/scheduled/{serie:path}/panel')
-            ->bootUsing(fn () => static::setupFilamentComponent())
-            ->homeUrl(fn () => app()->getCurrentScheduledConference()?->getHomeUrl())
+            ->bootUsing(fn() => static::setupFilamentComponent())
+            ->homeUrl(fn() => app()->getCurrentScheduledConference()?->getHomeUrl())
             ->discoverResources(in: app_path('Panel/ScheduledConference/Resources'), for: 'App\\Panel\\ScheduledConference\\Resources')
             ->discoverPages(in: app_path('Panel/ScheduledConference/Pages'), for: 'App\\Panel\\ScheduledConference\\Pages')
             ->discoverWidgets(in: app_path('Panel/ScheduledConference/Widgets'), for: 'App\\Panel\\ScheduledConference\\Widgets')
             ->discoverLivewireComponents(in: app_path('Panel/ScheduledConference/Livewire'), for: 'App\\Panel\\ScheduledConference\\Livewire')
             ->renderHook(
                 PanelsRenderHook::TOPBAR_START,
-                fn () => view('panel.scheduledConference.hooks.topbar'),
+                fn() => view('panel.scheduledConference.hooks.topbar'),
             )
             ->renderHook(
                 PanelsRenderHook::SIDEBAR_NAV_START,
@@ -82,8 +82,8 @@ class PanelProvider extends ServiceProvider
             ->id(static::PANEL_CONFERENCE)
             ->default()
             ->path('{conference:path}/panel')
-            ->bootUsing(fn () => static::setupFilamentComponent())
-            ->homeUrl(fn () => route('livewirePageGroup.conference.pages.home', ['conference' => app()->getCurrentConference()]))
+            ->bootUsing(fn() => static::setupFilamentComponent())
+            ->homeUrl(fn() => route('livewirePageGroup.conference.pages.home', ['conference' => app()->getCurrentConference()]))
             ->discoverResources(in: app_path('Panel/Conference/Resources'), for: 'App\\Panel\\Conference\\Resources')
             ->discoverPages(in: app_path('Panel/Conference/Pages'), for: 'App\\Panel\\Conference\\Pages')
             ->discoverWidgets(in: app_path('Panel/Conference/Widgets'), for: 'App\\Panel\\Conference\\Widgets')
@@ -93,14 +93,14 @@ class PanelProvider extends ServiceProvider
             ])
             ->renderHook(
                 PanelsRenderHook::TOPBAR_START,
-                fn () => view('panel.conference.hooks.topbar'),
+                fn() => view('panel.conference.hooks.topbar'),
             )
             ->renderHook(
                 PanelsRenderHook::SIDEBAR_NAV_START,
                 function () {
                     $currentConference = app()->getCurrentConference();
                     $conferenceQuery = Conference::query()
-                        ->when(!auth()->user()->hasRole(UserRole::Admin), fn ($query) => $query->whereHas('conferenceUsers', function ($query) {
+                        ->when(!auth()->user()->hasRole(UserRole::Admin), fn($query) => $query->whereHas('conferenceUsers', function ($query) {
                             $query->where('model_has_roles.model_id', auth()->id());
                         }))
                         ->where('path', '!=', $currentConference->path)
@@ -132,15 +132,15 @@ class PanelProvider extends ServiceProvider
         $this->setupPanel($panel)
             ->id(static::PANEL_ADMINISTRATION)
             ->path('administration')
-            ->homeUrl(fn () => route('livewirePageGroup.website.pages.home'))
-            ->bootUsing(fn () => static::setupFilamentComponent())
+            ->homeUrl(fn() => route('livewirePageGroup.website.pages.home'))
+            ->bootUsing(fn() => static::setupFilamentComponent())
             ->discoverResources(in: app_path('Panel/Administration/Resources'), for: 'App\\Panel\\Administration\\Resources')
             ->discoverPages(in: app_path('Panel/Administration/Pages'), for: 'App\\Panel\\Administration\\Pages')
             ->discoverWidgets(in: app_path('Panel/Administration/Widgets'), for: 'App\\Panel\\Administration\\Widgets')
             ->discoverLivewireComponents(in: app_path('Panel/Administration/Livewire'), for: 'App\\Panel\\Administration\\Livewire')
             ->renderHook(
                 PanelsRenderHook::SIDEBAR_NAV_START,
-                fn () => view('panel.administration.hooks.sidebar-nav-start'),
+                fn() => view('panel.administration.hooks.sidebar-nav-start'),
             )
             ->middleware(static::getMiddleware(), true)
             ->authMiddleware(static::getAuthMiddleware(), true);
@@ -156,23 +156,26 @@ class PanelProvider extends ServiceProvider
     {
         return $panel
             ->maxContentWidth('full')
+            ->when(!app()->isProduction(), fn(Panel $panel) => $panel->renderHook(
+                PanelsRenderHook::FOOTER,
+                fn() => Blade::render('<x-livewire-handle-error />')))
             ->renderHook(
                 PanelsRenderHook::FOOTER,
-                fn () => Blade::render(<<<'Blade'
+                fn() => Blade::render(<<<'Blade'
                         <x-footer-platform-panel />
                     Blade)
             )
             ->renderHook(
                 PanelsRenderHook::SCRIPTS_BEFORE,
-                fn () => Blade::render(<<<'Blade'
+                fn() => Blade::render(<<<'Blade'
                         @vite(['resources/panel/js/panel.js'])
                     Blade)
             )
             ->renderHook(
                 PanelsRenderHook::USER_MENU_PROFILE_AFTER,
-                function() {
+                function () {
                     $languages = Setting::get('languages', ['en']);
-                    if(count($languages) < 2){
+                    if (count($languages) < 2) {
                         return;
                     }
 
@@ -186,7 +189,7 @@ class PanelProvider extends ServiceProvider
             ])
             ->userMenuItems([
                 'profile' => MenuItem::make()
-                    ->url(fn (): string => Profile::getUrl()),
+                    ->url(fn(): string => Profile::getUrl()),
             ])
             ->darkMode(false)
             ->databaseNotifications()
@@ -196,15 +199,15 @@ class PanelProvider extends ServiceProvider
     public function register(): void
     {
         Filament::registerPanel(
-            fn (): Panel => $this->scheduledConferencePanel(Panel::make()),
+            fn(): Panel => $this->scheduledConferencePanel(Panel::make()),
         );
 
         Filament::registerPanel(
-            fn (): Panel => $this->conferencePanel(Panel::make()),
+            fn(): Panel => $this->conferencePanel(Panel::make()),
         );
 
         Filament::registerPanel(
-            fn (): Panel => $this->administrationPanel(Panel::make()),
+            fn(): Panel => $this->administrationPanel(Panel::make()),
         );
     }
 
@@ -247,7 +250,7 @@ class PanelProvider extends ServiceProvider
                 ->imageResizeUpscale(false)
                 ->maxSize(config('media-library.max_file_size') / 1024)
                 ->acceptedFileTypes(collect(config('media-library.accepted_file_types'))
-                    ->map(fn ($ext) => MimeType::fromExtension($ext) ?? $ext)
+                    ->map(fn($ext) => MimeType::fromExtension($ext) ?? $ext)
                     ->toArray());
         });
         DatePicker::configureUsing(function (DatePicker $datePicker): void {
