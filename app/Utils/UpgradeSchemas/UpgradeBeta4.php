5ac7b0465514575c7218b729a3e47be56a855260
Satria Salim <satriasalim005@gmail.com>
2024-08-29 09:07:45 +0800

    fix beta 4 version upgrader script

--- app/Utils/UpgradeSchemas/UpgradeBeta4.php
+++ app/Utils/UpgradeSchemas/UpgradeBeta4.php
@@ -4,35 +4,56 @@ namespace App\Utils\UpgradeSchemas;
 
 use App\Actions\MailTemplates\MailTemplatePopulateDefaultData;
 use App\Models\Role;
-use App\Models\Permission;
-use App\Models\Enums\UserRole;
-use Illuminate\Support\Facades\Artisan;
 use App\Actions\Roles\RoleAssignDefaultPermissions;
 use App\Actions\Permissions\PermissionPopulateAction;
 use App\Models\Conference;
 use App\Models\ScheduledConference;
+use App\Models\Submission;
+use App\Models\RegistrationType;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 class UpgradeBeta4 extends UpgradeBase
 {
 	public function run(): void
 	{
-		PermissionPopulateAction::run();
-
-		Role::lazy()->each(function (Role $role) {
-			RoleAssignDefaultPermissions::run($role);
-		});
+		$this->insertColumns();
 
 		Conference::lazy()->each(function (Conference $conference) {
 			MailTemplatePopulateDefaultData::run($conference);
 		});
 
 		ScheduledConference::lazy()->each(function (ScheduledConference $scheduledConference) {
-			$scheduledConference->setManyMeta([
-				'timezone' => 'UTC',
-				'submission_payment' => true,
-			]);
+			$scheduledConference->setManyMeta($this->getMetadata());
 		});
 
-		Artisan::call('migrate');
+		PermissionPopulateAction::run();
+
+		Role::lazy()->each(function (Role $role) {
+			RoleAssignDefaultPermissions::run($role);
+		});
+	}
+
+	public function getMetadata(): array
+	{
+		return [
+			'timezone' => 'UTC',
+			'submission_payment' => true,
+		];
+	}
+
+	public function insertColumns(): void
+	{
+		if(!Schema::hasColumn('registrations', (new Submission())->getForeignKey())) {
+            Schema::table('registrations', function (Blueprint $table) {
+                $table->foreignIdFor(Submission::class)->nullable()->constrained()->cascadeOnDelete();
+            });
+        }
+        
+        if(!Schema::hasColumn('registration_payments', 'level')) {
+            Schema::table('registration_payments', function (Blueprint $table) {
+                $table->unsignedInteger('level')->default(RegistrationType::LEVEL_PARTICIPANT);
+            });
+        }
 	}
 }	
