fd92724419e47a2531d16d3d0b7f22d024dbcbc9
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-09-05 16:40:45 +0800

    upgrade script for beta 5

--- app/Utils/UpgradeSchemas/UpgradeBeta5.php
+++ app/Utils/UpgradeSchemas/UpgradeBeta5.php
@@ -3,7 +3,9 @@
 namespace App\Utils\UpgradeSchemas;
 
 use App\Actions\MailTemplates\MailTemplatePopulateDefaultData;
+use App\Actions\Roles\RolePopulateScheduledConferenceAction;
 use App\Models\Conference;
+use App\Models\Enums\UserRole;
 use App\Models\ScheduledConference;
 use App\Models\Submission;
 use App\Models\RegistrationType;
@@ -15,9 +17,27 @@ class UpgradeBeta5 extends UpgradeBase
 {
 	public function run(): void
 	{
+		
+
 		Role::query()
 			->withoutGlobalScopes()
 			->lazy()
 			->each(fn($role) => $role->permissions()->detach());
+
+		Role::query()
+			->withoutGlobalScopes()
+			->whereIn('name', ['Series Manager'])
+			->lazy()
+			->each(fn($role) => $role->delete());
+
+		Role::query()
+			->withoutGlobalScopes()
+			->where('name', 'Conference Editor')
+			->update(['name' => 'Scheduled Conference Editor']);
+
+		ScheduledConference::withoutGlobalScopes()
+			->lazy()
+			->each(fn($scheduledConference) => RolePopulateScheduledConferenceAction::run($scheduledConference));
+		
 	}
-}	
+}
