e733a06c796e98f114b9383a8d8b101167d183af
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-11-01 09:31:53 +0800

    allow install plugin from zip

--- app/Panel/Administration/Pages/PluginManagement.php
+++ app/Panel/Administration/Pages/PluginManagement.php
@@ -12,6 +12,13 @@ use Filament\Infolists\Contracts\HasInfolists;
 use Filament\Infolists\Infolist;
 use Filament\Pages\Page;
 use Filament\Infolists\Components\Tabs;
+use App\Facades\Plugin as PluginFacade;
+use App\Models\Plugin;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Filament\Notifications\Notification;
+use Filament\Support\Enums\MaxWidth;
+use Illuminate\Support\Facades\Log;
 
 class PluginManagement extends Page implements HasForms, HasInfolists
 {
@@ -32,6 +39,49 @@ class PluginManagement extends Page implements HasForms, HasInfolists
     public static function getNavigationLabel(): string
     {
         return __('general.plugin');
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('upload-plugin')
+                ->label(__('general.upload_plugin'))
+                ->modalHeading(__('general.upload_plugin'))
+                ->visible(fn() => auth()->user()->can('install', Plugin::class))
+                ->form([
+                    FileUpload::make('file')
+                        ->label(__('general.file'))
+                        ->disk('plugins-tmp')
+                        ->acceptedFileTypes(['application/zip'])
+                        ->required(),
+                ])
+                ->modalWidth(MaxWidth::ExtraLarge)
+                ->action(function (array $data) {
+
+                    try {
+                        PluginFacade::install(PluginFacade::getTempDisk()->path($data['file']));
+                    } catch (\Throwable $th) {
+                        Notification::make('install-failed')
+                            ->danger()
+                            ->title(__('general.failed_to_install_plugin'))
+                            ->send();
+                        Log::error($th);
+
+                        return;
+                    } finally {
+                        PluginFacade::getTempDisk()->delete($data['file']);
+                    }
+
+                    $this->dispatch('refresh-table');
+                    
+                    Notification::make('install-success')
+                        ->title(__('general.install_success'))
+                        ->success()
+                        ->body(__('general.plugin_installed_successfully'))
+                        ->send();
+                })
+                ->modalSubmitActionLabel(__('general.submit')),
+        ];
     }
 
     public function infolist(Infolist $infolist): Infolist
